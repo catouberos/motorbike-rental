60e59ad596ceb2eee7bb6a587ffc4b589b891da9
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-09-13 22:55:49 +0700

    feat: implement Account

--- src/model/Account.cpp
+++ src/model/Account.cpp
@@ -1,36 +1,21 @@
+#include <sstream>
+
 #include "Account.h"
-void Account::printAccount()
+
+Account::Account(unsigned int id, std::string username, std::string password)
+    : Entity(id), username_(username), password_(password) {}
+
+std::string Account::toString()
 {
-    std::cout << "Username: " << m_username << '\n';
-    std::cout << "password: " << m_password << '\n';
+    std::ostringstream oss;
+
+    oss << "Username: " << username_ << std::endl
+        << "Password: " << password_;
+
+    return oss.str();
 }
 
-void Member::printMember()
+std::string Account::serialize()
 {
-    std::cout << "Member ID: " << m_memberID << '\n';
-    std::cout << "fullname: " << m_fullName << '\n';
-    std::cout << "ID type: " << m_idType << '\n';
-    std::cout << "ID number: " << m_idNumber << '\n';
-    std::cout << "License number: " << m_licenseNumber << '\n';
-    std::cout << "Credit point: " << m_creditPoint << '\n';
-    std::cout << "Renter rating: " << m_renterRating << '\n';
-}
-
-void Member::printMemberAdmin()
-{
-    Account::printAccount();
-    std::cout << "Member ID: " << m_memberID << '\n';
-    std::cout << "fullname: " << m_fullName << '\n';
-    std::cout << "ID type: " << m_idType << '\n';
-    std::cout << "ID number: " << m_idNumber << '\n';
-    std::cout << "License number: " << m_licenseNumber << '\n';
-    std::cout << "Credit point: " << m_creditPoint << '\n';
-    std::cout << "Renter rating: " << m_renterRating << '\n';
-}
-
-std::string Member::toFile()
-{
-    return Account::m_username + ',' + Account::m_password + ',' + m_fullName + ',' + m_idType + ',' + std::to_string(m_idNumber) + ','
-        + std::to_string(m_licenseNumber) + ',' + std::to_string(m_creditPoint) + ',' + std::to_string(m_renterRating) + ','
-        + std::to_string(m_ownMotorbikeID) + ',' + std::to_string(m_rentMotorbikeID) + ',' + std::to_string(m_memberID) + '\n';
+    return username_ + "," + password_;
 }
--- src/model/Account.h
+++ src/model/Account.h
@@ -1,67 +1,23 @@
-#include <iostream>
+#ifndef ACCOUNT_H_
+#define ACCOUNT_H_
+
 #include <string>
-#include <vector>
-#include <fstream>
-#ifndef ACCOUNT_H
-#define ACCOUNT_H
-class Account
+
+#include "Entity.h"
+
+class Account : public Entity
 {
-    std::string m_username{""};
-    std::string m_password{""};
+protected:
+    std::string username_;
+    std::string password_;
 
 public:
-    Account() = default;
-    Account(std::string username, std::string password) 
-        :m_username{username}, m_password{password}{}
-    
-    void printAccount();
-    friend class System;
-    friend class Member;
+    Account(unsigned int id, std::string username, std::string password);
 
-};
-
-class Member: public Account
-{   
-    int m_memberID{};
-    std::string m_fullName{""};
-    std::string m_idType{""};
-    int m_idNumber{};
-    int m_licenseNumber{};
-    int m_creditPoint{};
-    int m_ownMotorbikeID{};
-    int m_rentMotorbikeID{};
-    double m_renterRating{};
-
-public:
-    Member() = default;
-    Member
-    (
-        std::string username,
-        std::string password,
-        std::string fullName,
-        std::string idType,
-        int idNumber,
-        int licenseNumber,
-        int creditPoint,
-        double renterRating,
-        int ownMotorbikeID,
-        int rentMotorbikeID,
-        int memberID 
-    )
-    : Account{username, password}
-        , m_fullName{fullName}, m_idType{idType}
-        , m_idNumber{idNumber}, m_licenseNumber{licenseNumber}
-        , m_creditPoint{creditPoint}, m_ownMotorbikeID{ownMotorbikeID}
-        , m_renterRating{renterRating}, m_rentMotorbikeID{rentMotorbikeID}, m_memberID{memberID}{}
+    virtual std::string toString();
+    virtual std::string serialize();
 
     friend class System;
- 
-
-    void printMember();
-
-    void printMemberAdmin();
-
-    std::string toFile();
-
 };
+
 #endif
