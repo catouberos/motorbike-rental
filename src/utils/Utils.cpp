e81c2180b1e0613f8389ad3bc17c9944d0f28333
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-09-15 00:49:34 +0700

    feat: implement utils

---
+++ src/utils/Utils.cpp
@@ -0,0 +1,6 @@
+#include "Utils.h"
+
+void Utils::clrscr()
+{
+    std::cout << "\033[2J\033[1;1H";
+}
---
+++ src/utils/Utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <iostream>
+
+class Utils
+{
+public:
+    static void clrscr();
+};
+
+#endif
